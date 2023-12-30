e87993dc70047be3569d260670564da008374a72
Mister Ky <140906950+Mister-Ky@users.noreply.github.com>
2023-12-30 21:53:03 +0200

    Update Button.cpp

--- src/MKCDE/Graphics/Button.cpp
+++ src/MKCDE/Graphics/Button.cpp
@@ -2,13 +2,22 @@
 
 namespace MK
 {
-Button::Button()
+Button::Button() = default;
+
+Button::Button(sf::RectangleShape& shape, sf::Text& text)
 {
-  
+  this->m_shape = shape;
+  this->m_text = text;
 }
 
 Button::~Button()
 {
   
+}
+
+void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+  target.draw(m_shape, states);
+  target.draw(m_text, states);
 }
 }
