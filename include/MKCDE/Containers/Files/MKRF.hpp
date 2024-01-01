f3490e58b2a3bb6c51f0f800be4273b0fbb06f24
Mister Ky <140906950+Mister-Ky@users.noreply.github.com>
2024-01-01 23:04:38 +0200

    Update MKRF.hpp

--- include/MKCDE/Containers/Files/MKRF.hpp
+++ include/MKCDE/Containers/Files/MKRF.hpp
@@ -1,6 +1,36 @@
-////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 // MKRF - Mister KygecHik Resource Files
-////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//  -------------------------------------------------
+//  The structure of the MKRF file
+//  -------------------------------------------------
+//  Byte Offset    |  Size (bytes)  |  Description
+//  -------------------------------------------------
+//  0-3            |  4             |  MKRF signature (4D 4B 52 46)
+//  4-11           |  8             |  File version
+//  12             |  1             |  Encryption flag (0x00 - no encryption, 0x01 - EAS, etc.)
+//  13             |  1             |  Compression flag (0x00 - no compression, 0x01 - deflate, etc.)
+//  14-22          |  9             |  Checksum (CRC32)
+//  23-32          |  10            |  Number of resources
+//  33-42          |  10            |  Total size of all resources (in bytes)
+//  43-52          |  10            |  Reserved for future use
+//  53-64          |  12            |  End-of-file marker
+//  65+            |  Variable      |  Resource files
+//
+//
+//  -------------------------------------------------
+//  The structure of the resource file
+//  -------------------------------------------------
+//  Byte Offset    |  Size (bytes)  |  Description
+//  -------------------------------------------------
+//  0-255          |  256           |  Resource name
+//  256-259        |  4             |  Resource size (in bytes)
+//  260            |  1             |  Empty file flag (0x01 if empty, 0x00 otherwise)
+//  261+           |  Variable      |  Resource data
+//  Variable-12    |  12            |  End-of-resource marker
+////////////////////////////////////////////////////////////////////////////////////////
 
 #include <fstream>
 #include <vector>
