82f43f675cc056d1be3546939edcb88ae9a415bd
Mister Ky <afanasievsergiy3524@gmail.com>
2024-01-08 18:59:09 +0200

    3 раз забыл

--- include/MKCDE/Graphics/Triangle.hpp
+++ include/MKCDE/Graphics/Triangle.hpp
@@ -15,6 +15,8 @@ public:
   ~Triangle();
 
   void setPoints(const sf::Vector2f& point1, const sf::Vector2f& point2, const sf::Vector2f& point3);
+private:
+  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
 }
 
