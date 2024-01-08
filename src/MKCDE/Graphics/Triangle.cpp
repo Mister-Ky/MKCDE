fb66091a3d3d25e3ad4a5ffc5c1d60dd49dca5e7
Mister Ky <afanasievsergiy3524@gmail.com>
2024-01-08 17:41:48 +0200

    Update Triangle.cpp

--- src/MKCDE/Graphics/Triangle.cpp
+++ src/MKCDE/Graphics/Triangle.cpp
@@ -4,9 +4,8 @@ namespace MK
 {
 Triangle::Triangle() = default;
 
-Triangle::Triangle(const sf::Vector2f& point1, const sf::Vector2f& point2, const sf::Vector2f& point3)
+Triangle::Triangle(const sf::Vector2f& point1, const sf::Vector2f& point2, const sf::Vector2f& point3) : m_shape(3)
 {
-  m_shape.setPointCount(3);
   m_shape.setPoint(0, point1);
   m_shape.setPoint(1, point2);
   m_shape.setPoint(2, point3);
