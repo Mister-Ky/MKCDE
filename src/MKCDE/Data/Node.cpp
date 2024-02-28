0404afce2369a596a82ae0dad4adc0f74a343172
Mister Ky <afanasievsergiy3524@gmail.com>
2024-02-28 16:07:37 +0200

    Update Node.cpp

--- src/MKCDE/Data/Node.cpp
+++ src/MKCDE/Data/Node.cpp
@@ -4,14 +4,7 @@ MK::Node::Node(NodeID id) : id(id) {}
 
 void MK::Node::add_child(std::shared_ptr<Node> child)
 {
-    if (children.empty()) 
-    {
-        children[1] = child;
-    }
-    else
-    {
-        children[children.size() + 1] = child;
-    }
+    children[children.size() + 1] = child;
 }
 
 void MK::Node::add_child(NodeLevel level, std::shared_ptr<Node> child)
