4175b739f18445cac23b1ca65255ccd6ac89ce7c
Mister Ky <afanasievsergiy3524@gmail.com>
2024-03-08 15:28:42 +0200

    Update MKRF.hpp

--- include/MKCDE/Data/MKRF.hpp
+++ include/MKCDE/Data/MKRF.hpp
@@ -11,12 +11,13 @@
 //  4-11           |  8             |  File version
 //  12             |  1             |  Encryption flag (0x00 - no encryption, 0x01 - EAS, etc.)
 //  13             |  1             |  Compression flag (0x00 - no compression, 0x01 - deflate, etc.)
-//  14-22          |  9             |  Checksum (CRC32)
-//  23-32          |  10            |  Number of resources
-//  33-42          |  10            |  Total size of all resources (in bytes)
-//  43-52          |  10            |  Reserved for future use
-//  53-64          |  12            |  End-of-file marker
-//  65+            |  Variable      |  Resource files
+//  14-15          |  2             |  Checksum flag (0x00 - CRC32, 0x01 - CRC64, etc.)
+//  16-Variable    |  Variable      |  Checksum (Size is determined by the Checksum flag)
+//  prev-(prev+10) |  10            |  Number of resources
+//  prev-(prev+10) |  10            |  Total size of all resources (in bytes)
+//  prev-(prev+10) |  10            |  Reserved for future use
+//  prev-(prev+12) |  12            |  End-of-file marker
+//  prev+          |  Variable      |  Resource files
 //
 //
 //  -------------------------------------------------
@@ -28,7 +29,7 @@
 //  256-259        |  4             |  Resource size (in bytes)
 //  260            |  1             |  Empty file flag (0x01 if empty, 0x00 otherwise)
 //  261-272        |  12            |  Reserved for future use
-//  273+           |  Min of 1 byte |  Resource data
+//  273+           |  Variable      |  Resource data (Min of 1 byte)
 //  Last 12        |  12            |  End-of-resource marker
 ////////////////////////////////////////////////////////////////////////////////////////
 
