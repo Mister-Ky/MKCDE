f35a2335750415068ee574014fbdf28c4b1e21cf
Mister Ky <afanasievsergiy3524@gmail.com>
2024-03-22 15:01:02 +0200

    Update App.hpp

--- include/MKCDE/Application/App.hpp
+++ include/MKCDE/Application/App.hpp
@@ -3,17 +3,23 @@
 #ifndef MK_APP_HPP
 #define MK_APP_HPP
 
+#include <MKCDE/Data/NodeTree.hpp>
+#include <SFML/System/NonCopyable.hpp>
+#include <Windows.h>
+
 namespace mk
 {
-class App
+class App : public sf::NonCopyable
 {
 public:
 	App();
 	virtual ~App();
 
-	virtual void init() = 0;
+	virtual byte init() = 0;
 	virtual void run() = 0;
 	virtual void shutdown() = 0;
+protected:
+	NodeTree tree;
 };
 }
 
