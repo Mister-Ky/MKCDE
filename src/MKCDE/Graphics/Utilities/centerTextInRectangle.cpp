42af0cedad36af97dbf591b28ff98f4497f2a2a1
Mister Ky <afanasievsergiy3524@gmail.com>
2024-05-09 17:15:41 +0300

    Update centerTextInRectangle.cpp

--- src/MKCDE/Graphics/Utilities/centerTextInRectangle.cpp
+++ src/MKCDE/Graphics/Utilities/centerTextInRectangle.cpp
@@ -6,7 +6,7 @@ void mk::centerTextInRectangle(sf::RectangleShape& rectangle, sf::Text& text)
     sf::FloatRect textBounds = text.getGlobalBounds();
 
     float x = rectBounds.left + (rectBounds.width / 2.0f) - (textBounds.width / 2.0f);
-    float y = rectBounds.top + (rectBounds.height / 2.0f) - (text.getCharacterSize() / 2.0f);
+    float y = rectBounds.top + (rectBounds.height / 2.0f) - (textBounds.height / 2.0f);
 
     text.setPosition(x, y);
 }
