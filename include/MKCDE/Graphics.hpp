c6e9fc079dfd19ffc018aed94fc4523904fdd0bd
Mister Ky <afanasievsergiy3524@gmail.com>
2024-06-14 11:33:38 +0300

    update macros c++

--- include/MKCDE/Graphics.hpp
+++ include/MKCDE/Graphics.hpp
@@ -3,13 +3,19 @@
 #ifndef MKCDE_GRAPHICS_HPP
 #define MKCDE_GRAPHICS_HPP
 
-#include <imgui.h>
-#include <imgui-sfml.h>
 #include <MKCDE/Graphics/Button.hpp>
 #include <MKCDE/Graphics/Slider.hpp>
 #include <MKCDE/Graphics/Triangle.hpp>
 #include <MKCDE/Graphics/Utils/centerTextInRectangle.hpp>
 #include <MKCDE/System.hpp>
 #include <SFML/Graphics.hpp>
+
+#if defined(MKCDE_ENABLE_IMGUI_SFML)
+#include <imgui.h>
+#include <imgui-sfml.h>
+#endif
+#if defined(MKCDE_ENABLE_TGUI)
+#include <TGUI/TGUI.hpp>
+#endif
 
 #endif
